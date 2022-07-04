77618e6ce293e12ed967cf790afa0b90143467e4
Sergejs Germanovs <sergejs.germanovs@edigard.com>
2022-07-04 11:58:38 +0300

    Set up boxes creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,47 @@
+import { useState, useEffect } from 'react'
+
 import './App.css'
 
 const App = () => {
 
+  const [ prisoner, setPrisoner ] = useState(0);
+  const [ amount, setAmount ] = useState(10);
+  const [ boxes, setBoxes ] = useState([]);
+
+  const shuffleArray = array => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    return array;
+  }
+
+  useEffect(() => {
+    let boxes = [ ...Array(amount).keys() ];
+    setBoxes(shuffleArray(boxes));
+  }, []);
+
   return <>
-    100 prisoners
+    <h3>100 prisoners</h3>
+
+    <div className="boxes">
+
+      {
+        boxes.map((value, index) => (
+          <div className="box" key={index}>
+            <div className="box-number"><b>{ index + 1 }</b></div>
+            <div className="box-inside-number">{ value }</div>
+          </div>
+        ))
+      }
+
+    </div>
+
+    <div className="stats">
+      Current prisoner number: <b>{ prisoner + 1 } of { amount }</b>
+    </div>
   </>;
 
 }
